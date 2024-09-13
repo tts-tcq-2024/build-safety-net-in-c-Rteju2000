3ae922c46e4aaba028c76a030551cf35525c8645
Rteju2000 <tejashwinir15@gmail.com>
2024-09-13 09:53:45 +0530

    Update Soundex.h

--- Soundex.h
+++ Soundex.h
@@ -1,39 +1,42 @@
-#ifndef SOUNDEX_H
-#define SOUNDEX_H
 
+    #ifndef SOUNDEX_H
+#define SOUNDEX_H
+ 
+#include "Soundex.h"
 #include <ctype.h>
 #include <string.h>
-
-const char* getSoundexCode(char c) {
-    static const char soundexTable[26] = {
-        '0', '1', '2', '3', '0', '1', '2', '0', '0', '2', '2', '4', '5', '5', 
-        '0', '1', '2', '6', '2', '3', '0', '1', '0', '2', '0', '2'
-    };
+ 
+char getSoundexCode(char c) {
     c = toupper(c);
-    return (c >= 'A' && c <= 'Z') ? soundexTable[c - 'A'] : '0';
+    switch (c) {
+        case 'B': case 'F': case 'P': case 'V': return '1';
+        case 'C': case 'G': case 'J': case 'K': case 'Q': case 'S': case 'X': case 'Z': return '2';
+        case 'D': case 'T': return '3';
+        case 'L': return '4';
+        case 'M': case 'N': return '5';
+        case 'R': return '6';
+        default: return '0'; // For A, E, I, O, U, H, W, Y
+    }
 }
-
+ 
 void generateSoundex(const char *name, char *soundex) {
+    int len = strlen(name);
     soundex[0] = toupper(name[0]);
     int sIndex = 1;
-
-    for (int i = 1; name[i] != '\0' && sIndex < 4; i++) {
+ 
+    for (int i = 1; i < len && sIndex < 4; i++) {
         char code = getSoundexCode(name[i]);
         if (code != '0' && code != soundex[sIndex - 1]) {
             soundex[sIndex++] = code;
         }
     }
-
+ 
     while (sIndex < 4) {
         soundex[sIndex++] = '0';
     }
-
+ 
     soundex[4] = '\0';
 }
-
+ 
 #endif // SOUNDEX_H
-
-
-
-
 
